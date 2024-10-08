32ebda03bbe25b0c29f7b6e3c9d01dde9b96335f
Anilkumar83 <168853846+Anilkumar83@users.noreply.github.com>
2024-10-08 14:53:44 +0530

    Update tb_half_adder.v

--- tb_half_adder.v
+++ tb_half_adder.v
@@ -1,7 +1,21 @@
-module half_adder(
-    input a, b,
-    output sum, carry
-);
-    assign sum = a ^ b;
-    assign carry = a & b;
+module tb_half_adder();
+    reg a, b;
+    wire sum, carry;
+
+    half_adder UUT (
+        .a(a), 
+        .b(b), 
+        .sum(sum), 
+        .carry(carry)
+    );
+
+    initial begin
+        $dumpfile("waveform.vcd");
+        $dumpvars(0, tb_half_adder);
+        a = 0; b = 0; #10;
+        a = 0; b = 1; #10;
+        a = 1; b = 0; #10;
+        a = 1; b = 1; #10;
+        $stop;
+    end
 endmodule
